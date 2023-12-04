16b96401196edd8f9e13b47c85d01d753a1852b2
Giorgos <gm7.gavroleon@gmail.com>
2023-12-04 21:39:45 +0200

    Trivial commit

--- todo/models.py
+++ todo/models.py
@@ -4,10 +4,31 @@ from django.utils import timezone
 
 # Create your models here.
 class Task(models.Model):
+    TYPE_CHOICES = (
+        ("py", "Python programming"),
+        ("cpp", "C++ programming"),
+        ("mldl", "ML/DL"),
+        ("django", "django web development"),
+        ("study", "Studying"),
+        ("trade", "Trading"),
+        ("fluid", "Fluid dynamics"),
+        ("thermodynamics", "Thermodynamics"),
+        ("maths", "Mathematics"),
+        ("ices", "ICEs"),
+        ("other", "Other"),
+    )
+
     STATUS_CHOICES = (
         ("open", "OPEN"),
         ("closed", "CLOSED"),
         ("withdrew", "WITHDREW"),
+    )
+
+    task_type_1 = models.CharField(
+        max_length=100, choices=TYPE_CHOICES, default="other", blank=True
+    )
+    task_type_2 = models.CharField(
+        max_length=100, choices=TYPE_CHOICES, default="other", blank=True
     )
     status = models.CharField(max_length=100, choices=STATUS_CHOICES, default="open")
     description = models.TextField()
