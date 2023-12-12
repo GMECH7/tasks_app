b67970af09d3598b2bab159bf93ee4dd6149354c
Giorgos <gm7.gavroleon@gmail.com>
2023-12-12 21:47:12 +0200

    Added task form

--- todo/urls.py
+++ todo/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.home, name="todo-home"),
+    path("", views.home, name="home"),
+    path("create-task/", views.createTask, name="create-task"),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Task
+from .forms import TaskForm
 
 
 def home(request):
     context = {"tasks": Task.objects.all()}
-    return render(request, "todo/home.html", context)
+    return render(request, "todo/task.html", context)
+
+
+def createTask(request):
+    form = TaskForm()
+    if request.method == "POST":
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+
+    context = {"form": form}
+    return render(request, "todo/task_form.html", context)
