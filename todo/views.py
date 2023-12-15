810af76c81ac9ea323e6b8ac258101d2a18cf801
Giorgos <gm7.gavroleon@gmail.com>
2023-12-15 19:46:53 +0200

    Added login, logout, registration etc.

--- todo/views.py
+++ todo/views.py
@@ -1,7 +1,60 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Task
 from .forms import TaskForm
+
+
+def loginPage(request):
+    page = "login"
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "POST":
+        username = request.POST.get("username").lower()
+        password = request.POST.get("password")
+        try:
+            user = User.objects.get(username=username)
+        except:
+            messages.error(request, "User does not exist")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "Username OR password does not exist")
+
+    context = {"page": page}
+    return render(request, "todo/login_register.html", context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect("home")
+
+
+def registerPage(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            messages.success(request, "Account created successfully")
+            return redirect("home")
+        else:
+            messages.error(request, "An error occured during registration!!!")
+
+    context = {"form": form}
+    return render(request, "todo/login_register.html", context)
 
 
 def home(request):
@@ -9,6 +62,7 @@ def home(request):
     return render(request, "todo/task.html", context)
 
 
+@login_required(login_url="login")
 def createTask(request):
     form = TaskForm()
     if request.method == "POST":
@@ -21,6 +75,7 @@ def createTask(request):
     return render(request, "todo/task_form.html", context)
 
 
+@login_required(login_url="login")
 def updateTask(request, pk):
     task = Task.objects.get(id=pk)
     form = TaskForm(instance=task)
@@ -34,6 +89,7 @@ def updateTask(request, pk):
     return render(request, "todo/task_form.html", context)
 
 
+@login_required(login_url="login")
 def deleteTask(request, pk):
     task = Task.objects.get(id=pk)
     if request.method == "POST":
